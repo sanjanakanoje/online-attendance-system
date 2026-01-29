f2e9830e7b01298a789c80f503797ed136b535d5
Sanjana Kanoje <sanjana2852005@gmail.com>
2026-01-29 20:45:57 +0530

    Initial Spring Boot project setup

---
+++ attendance/src/test/java/com/sanjana/attendance/AttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjana.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
